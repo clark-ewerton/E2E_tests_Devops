a4c38cdcd928c8da671950e27a3c9ce7700c52ff
clark <clarkewerton@hotmail.com>
2023-10-12 21:03:10 -0300

    Ajustando para receber o selenium grid

--- src/test/java/com/automate/withme/testNgUtils/BrowserFactory.java
+++ src/test/java/com/automate/withme/testNgUtils/BrowserFactory.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -34,12 +35,13 @@ public class BrowserFactory {
 			
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\clark\\OneDrive\\Documentos\\chromedriver.exe");
 			webDriver.set(new ChromeDriver());
-			ChromeOptions options = new ChromeOptions();
-		    options.addArguments("--start-maximized");
-		    options.addArguments("--disable-infobars");
-//		DesiredCapabilities dc = DesiredCapabilities.chrome();
+//			ChromeOptions options = new ChromeOptions();
+//		    options.addArguments("--start-maximized");
+//		    options.addArguments("--disable-infobars");
+//			options.addArguments("--disable-dev-shm-usage");
+//			DesiredCapabilities dc = DesiredCapabilities.chrome();
 //		    dc.setCapability(ChromeOptions.CAPABILITY, options);
-//		webDriver.set( new RemoteWebDriver(new URL("http://192.168.26.1:4444/wd/hub"),dc));
+//		webDriver.set( new RemoteWebDriver(new URL("http://3.220.41.50:4444"),dc));
 
 			
 		}
@@ -55,11 +57,15 @@ public class BrowserFactory {
 		}
 		else if(browser.equalsIgnoreCase("FF")){
 			System.setProperty("webdriver.gecko.driver", "C:\\Users\\clark\\OneDrive\\Documentos\\geckodriver.exe");
+//			System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
 			 webDriver.set(new FirefoxDriver());
 			 
-			DesiredCapabilities capabilities = DesiredCapabilities.firefox(); 
-			 
-			// webDriver.set(new RemoteWebDriver(new URL("http://192.168.26.1:4444/wd/hub"),capabilities));
+//			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//			FirefoxOptions options = new FirefoxOptions();
+//			options.setHeadless(true); // Define o modo headless
+//			capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options);
+//
+//			 webDriver.set(new RemoteWebDriver(new URL("http://3.220.41.50:4444"),capabilities));
 		}
 	}
 
